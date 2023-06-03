299b6864445d4667d396bd9365a3d38477a33446
devCharlieP <rjsgud444@gmail.com>
2023-06-03 18:01:47 +0900

    feat:Unsup_BERT_v1_fine_tuning

--- new_model/UnSup_Bert_V1/finetuning.py
+++ new_model/UnSup_Bert_V1/finetuning.py
@@ -1,20 +1,14 @@
-from transformers import BertConfig, BertTokenizer, BertModel, get_scheduler
+from transformers import BertTokenizer, BertForSequenceClassification
 import torch
-from torch.optim import Adam, AdamW
+from torch.optim import AdamW
 from torch.utils.data import DataLoader, Dataset
 from torch.nn import CrossEntropyLoss
 from sklearn.model_selection import train_test_split
 import pandas as pd
-from tqdm.auto import tqdm
-import numpy as np
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-config = BertConfig.from_json_file("new_model/UnSup_Bert_V1/model_outputs/config.json")
-model_state_dict = torch.load("new_model/UnSup_Bert_V1/model_outputs/pytorch_model.bin")
-model = BertModel(config)
-model.load_state_dict(model_state_dict, strict=False)
-
+model = BertForSequenceClassification.from_pretrained("new_model/UnSup_Bert_V1/model_outputs", num_labels=3)
 tokenizer = BertTokenizer.from_pretrained("new_model/UnSup_Bert_V1/model_outputs")
 
 # Define custom dataset class
@@ -40,18 +34,24 @@ train_dataset, val_dataset = train_test_split(dataset, test_size=0.2, random_sta
 
 # Define dataloaders
 train_dataloader = DataLoader(train_dataset, batch_size=4, shuffle=True)
-val_dataloader = DataLoader(val_dataset, batch_size=4, shuffle=False)   
+val_dataloader = DataLoader(val_dataset, batch_size=4, shuffle=False)
 
-num_epochs = 10
+# Define loss function and optimizer
+loss_fn = CrossEntropyLoss()
 optimizer = AdamW(model.parameters(), lr=2e-5)
 
-loss_fn = torch.nn.CrossEntropyLoss()
-
+# Move model to device
 model.to(device)
 
+# Set model to train mode
 model.train()
 
+num_epochs = 5
+
+# Training loop
 for epoch in range(num_epochs):
+    train_loss = 0.0
+    train_accuracy = 0.0
     for batch in train_dataloader:
         input_ids, attention_mask, labels = batch
         input_ids = input_ids.to(device)
@@ -60,36 +60,51 @@ for epoch in range(num_epochs):
 
         optimizer.zero_grad()
 
-        outputs = model(input_ids=input_ids, attention_mask=attention_mask)
-        logits = outputs.last_hidden_state[:, 0, :]  # Assuming BERT model has a pooler layer
-        loss = loss_fn(logits, labels)
+        outputs = model(input_ids=input_ids, attention_mask=attention_mask, labels=labels)
+        loss = outputs.loss
+        logits = outputs.logits
+
+        train_loss += loss.item()
+
+        _, predicted_labels = torch.max(logits, dim=1)
+        correct_predictions = (predicted_labels == labels).sum().item()
+        train_accuracy += correct_predictions
 
         loss.backward()
         optimizer.step()
 
-model.eval()
+    train_loss /= len(train_dataset)
+    train_accuracy /= len(train_dataset)
+    
+    print(f"Epoch {epoch+1}/{num_epochs}")
+    print(f"Train Loss: {train_loss:.4f} | Train Accuracy: {train_accuracy:.4f}")
+    print("--------------------")      
 
-val_loss = 0.0
-val_accuracy = 0.0
+    # Validation loop
+    model.eval()
+    
+    val_loss = 0.0
+    val_accuracy = 0.0
+    
+    with torch.no_grad():
+        for batch in val_dataloader:
+            input_ids, attention_mask, labels = batch
+            input_ids = input_ids.to(device)
+            attention_mask = attention_mask.to(device)
+            labels = labels.to(device)
 
-with torch.no_grad():
-    for batch in val_dataloader:
-        input_ids, attention_mask, labels = batch
-        input_ids = input_ids.to(device)
-        attention_mask = attention_mask.to(device)
-        labels = labels.to(device)
+            outputs = model(input_ids=input_ids, attention_mask=attention_mask, labels=labels)
+            loss = outputs.loss
+            logits = outputs.logits
 
-        outputs = model(input_ids=input_ids, attention_mask=attention_mask)
-        logits = outputs.last_hidden_state[:, 0, :]  # Assuming BERT model has a pooler layer
-        loss = loss_fn(logits, labels)
-        val_loss += loss.item()
+            val_loss += loss.item()
 
-        _, predicted_labels = torch.max(logits, dim=1)
-        correct_predictions = (predicted_labels == labels).sum().item()
-        val_accuracy += correct_predictions
-
-val_loss /= len(val_dataset)
-val_accuracy /= len(val_dataset)
+            _, predicted_labels = torch.max(logits, dim=1)
+            correct_predictions = (predicted_labels == labels).sum().item()
+            val_accuracy += correct_predictions
+            val_loss /= len(val_dataset)
+            val_accuracy /= len(val_dataset)
 
 model.save_pretrained("fine_tuned_model")
 tokenizer.save_pretrained("fine_tuned_model")
+print("Complete fine-tuning!!!")
