e5f93918c8fe9d23549ac2139868ab18679457c8
devCharlieP <rjsgud444@gmail.com>
2023-06-18 00:44:35 +0900

    feat:no_final

---
+++ new_model/dataset/after/no_final/count.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+data = pd.read_csv("new_model/dataset/after/no_final/all_data_final.csv")
+
+label_counts = data['label'].value_counts()
+
+print(label_counts)
---
+++ new_model/dataset/after/no_final/dropout.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+# 데이터 로드
+data = pd.read_csv("new_model/dataset/after/no_final/all_data.csv")
+
+# 라벨이 0인 데이터 추출
+data_0 = data[data['label'] == 0]
+
+# 라벨이 0이 아닌 데이터 추출
+data_not_0 = data[data['label'] != 0]
+
+# 라벨이 0인 데이터에서 40% 샘플링
+sample_data_0 = data_0.sample(frac=0.4)
+
+# 라벨이 0인 샘플링된 데이터와 라벨이 0이 아닌 데이터 결합
+new_data = pd.concat([sample_data_0, data_not_0])
+
+# 새로운 데이터를 csv 파일로 저장
+new_data.to_csv("all_data_final.csv", index=False)
